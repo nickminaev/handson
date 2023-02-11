dec0dca19c14932dbe7d4e5210ac10733fe45602
nickminaev <nickgramru@gmail.com>
2023-02-11 21:38:20 +0200

    imrpovements to the Dockerfile

--- first-nodejs-server/server.js
+++ first-nodejs-server/server.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/fail', (req, res)=> {
+  res.status(500).send("A very nasty erro")
+  console.log("A very nasty error happened in the process. I'm exiting")
+  process.exit(1)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
